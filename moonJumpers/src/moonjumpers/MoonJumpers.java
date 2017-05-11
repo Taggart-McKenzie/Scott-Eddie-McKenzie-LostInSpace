18023ac6540806b2f2dec0cf4994235c5810015c
mckenzietaggart <mckenzietaggart@192.168.0.101>
2017-05-10 19:59:30 -0700

    5/10 - player class

---
+++ moonJumpers/src/moonjumpers/MoonJumpers.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moonjumpers;
+
+import byui.cit260.moonJumpers.model.Player;
+
+/**
+ *
+ * @author mckenzietaggart
+ */
+public class MoonJumpers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Player playerOne = new Player();
+        
+        playerOne.setName("Fred Flintsone");
+        playerOne.setBestTime(7.00);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+        
+        
+    }
+    
+}
