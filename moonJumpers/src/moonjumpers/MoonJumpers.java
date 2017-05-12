ebb7a3ed5bd6b58b6d3d06d3c1fbf86cddcbbcde
mckenzietaggart <mckenzietaggart@192.168.0.101>
2017-05-11 23:11:20 -0700

    5/11 - actor, game, weapon classes

--- moonJumpers/src/moonjumpers/MoonJumpers.java
+++ moonJumpers/src/moonjumpers/MoonJumpers.java
@@ -5,7 +5,10 @@
  */
 package moonjumpers;
 
+import byui.cit260.moonJumpers.model.Actor;
+import byui.cit260.moonJumpers.model.Game;
 import byui.cit260.moonJumpers.model.Player;
+import byui.cit260.moonJumpers.model.Weapon;
 
 /**
  *
@@ -17,6 +20,7 @@ public class MoonJumpers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //display the player test
         Player playerOne = new Player();
         
         playerOne.setName("Fred Flintsone");
@@ -25,7 +29,36 @@ public class MoonJumpers {
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
         
+        //display the actor test
+        Actor actorOne = new Actor();
         
+        actorOne.setName("Green Alien");
+        actorOne.setDescription("Fights against the player on the planet Jupiter");
+        actorOne.setCurrentLocation("Jupiter");
+
+        String actorInfo = actorOne.toString();
+        System.out.println(actorInfo);
+        
+        //display the weapon test
+        Weapon weaponOne = new Weapon();
+        
+        weaponOne.setName("Astro Blaster");
+        weaponOne.setDescription("The astro blaster has maximum strength and will kill an alien in one hit");
+        weaponOne.setStrength(20);
+        
+        String weaponInfo = weaponOne.toString();
+        System.out.println(weaponInfo);
+        
+        //display the game test
+        Game gameOne = new Game();
+        
+        gameOne.setTotalTime(10);
+        gameOne.setNumberOfPeople(1);
+        gameOne.setAttribute("You have visited Earth's Moon, Saturn, and the Sun");
+
+        String gameInfo = gameOne.toString();
+        System.out.println(gameInfo);
+
     }
     
 }
